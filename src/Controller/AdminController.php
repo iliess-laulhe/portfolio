cee06dab29b088aea17f49ae7d8babf76d449689
iliess laulhe <iliesslaulhe@icloud.com>
2022-02-04 10:39:09 +0100

    delete and edit skill and project

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Entity\Project;
 use App\Entity\Skill;
+use App\Form\ProjectType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="show")
      */
     public function index(ManagerRegistry $managerRegistry): Response
     {
@@ -22,5 +29,47 @@ class AdminController extends AbstractController
             'skills' => $skills,
             'projects' => $projects
         ]);
+    }
+    /**
+     * @Route("/adminEdit/{id}", name="edit")
+     */
+    public function edit(Request $request, ManagerRegistry $managerRegistry, Project $project):Response
+    {
+        $entityManager = $managerRegistry->getManager();
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($project);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_show');
+        }
+
+        return $this->render('admin/edit.html.twig', [
+            'form' => $form->createView(),
+            'project' => $project,
+        ]);
+    }
+
+    /**
+     * @Route("/deleteProject/{id}", name="deleteProject")
+     */
+    public function deleteProject(Project $project, ManagerRegistry $managerRegistry,  EntityManagerInterface $entityManager): Response
+    {
+        $entityManager = $managerRegistry->getManager();
+        $entityManager->remove($project);
+        $entityManager->flush();
+        return $this->redirectToRoute('admin_show');
+    }
+
+    /**
+     * @Route("/deleteProject/{id}", name="deleteSkill")
+     */
+    public function deleteSkill(Skill $skill, ManagerRegistry $managerRegistry,  EntityManagerInterface $entityManager): Response
+    {
+        $entityManager = $managerRegistry->getManager();
+        $entityManager->remove($skill);
+        $entityManager->flush();
+        return $this->redirectToRoute('admin_show');
     }
 }
